83081da8afb7a1369af334a093a5fbed9f17b782
sustechkl <12111012@mail.sustech.edu.cn>
2023-04-26 19:38:37 +0800

    temp.cpp

---
+++ Lab/temp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define For(_,l,r) for(int _ = l; _ <= r; ++_)
+#define _For(_,r,l) for(int _ = r; _ >= l; --_)
+template <class R> inline bool Checkmax(R &A, R B) {
+	return A < B ? A = B, 1 : 0;
+}
+template <class R> inline bool Checkmin(R &A, R B) {
+	return A > B ? A = B, 1 : 0;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
